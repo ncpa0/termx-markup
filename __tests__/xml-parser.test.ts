3931c27e720424407d5c05b9356e31ebfdd9306c
ncpa0cpl <sbretner@touchbistro.com>
2023-01-18 11:23:17 +0100

    test: more xml parser test unit cases

--- __tests__/xml-parser.test.ts
+++ __tests__/xml-parser.test.ts
@@ -1,4 +1,14 @@
-import { parseXml } from "../src/xml-parser";
+import { parseXml, XmlParserError } from "../src/xml-parser";
+
+const catchErr = (fn: () => void) => {
+  try {
+    fn();
+  } catch (err) {
+    return err as Error;
+  }
+};
+
+XmlParserError.prototype["getStack"] = jest.fn(() => "at ./index:1:1");
 
 describe("parseXml", () => {
   it("should parse simple xml", () => {
@@ -322,6 +332,62 @@ describe("parseXml", () => {
     });
   });
 
+  it("should correctly parse top-level self closing tag", () => {
+    const xml = "<p />";
+    const result = parseXml(xml);
+
+    expect(result).toEqual({
+      tag: "p",
+      textNode: false,
+      attributes: [],
+      content: [],
+    });
+  });
+
+  it("should correctly parse self closing tag", () => {
+    const xml = "<p>Hello<br/><br />World</p>";
+    const result = parseXml(xml);
+
+    expect(result).toEqual({
+      tag: "p",
+      textNode: false,
+      attributes: [],
+      content: [
+        "Hello",
+        { tag: "br", textNode: false, attributes: [], content: [] },
+        { tag: "br", textNode: false, attributes: [], content: [] },
+        "World",
+      ],
+    });
+  });
+
+  it("should correctly parse self closing tag with attributes", () => {
+    const xml = '<p>Hello<br id="1"/><br id="2" />World</p>';
+    const result = parseXml(xml);
+
+    expect(result).toEqual({
+      tag: "p",
+      textNode: false,
+      attributes: [],
+      content: [
+        "Hello",
+        {
+          tag: "br",
+          textNode: false,
+          attributes: [["id", "1"]],
+          content: [],
+        },
+        {
+          tag: "br",
+          textNode: false,
+          attributes: [["id", "2"]],
+          content: [],
+        },
+        "World",
+      ],
+    });
+  });
+
   describe("with invalid whitespaces in tags", () => {
     it("should ignore whitespace in open tag", () => {
       const xml = "<  p  >Hello</p>";
@@ -395,6 +461,152 @@ describe("parseXml", () => {
           "coorg",
         ],
       });
+    });
+  });
+
+  describe("negative scenarios", () => {
+    it("should correctly format the error message for multiline xml", () => {
+      const xml = `
+      <root>
+        <span>Hello</span>
+        <p>World</span>
+      </root>
+    `;
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error if a closing tag is missing", () => {
+      const xml = "<p>Hello";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. XML closing tag is missing. Expected a close tag for 'p' before the end of the document."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error if a closing tag is missing in a nested structure", () => {
+      const xml = "<span>Hello<p>World</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. XML closing tag is missing. Expected a close tag for 'span' before the end of the document."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error if the closing tag is not matching the opening tag", () => {
+      const xml = "<p>Hello</span>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Closing tag does not match opening tag, expected 'p' but found 'span'."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw when encountering / character inside a tag", () => {
+      const xml = "<p / >Hello</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Invalid character encountered."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw when encountering / character inside a tag between attributes", () => {
+      const xml = "<p bold / invert >Hello</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Invalid character encountered."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw when encountering / character inside a tag name", () => {
+      const xml = "<sp/an>Hello</sp/an>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Invalid character encountered."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error if the tag name is empty", () => {
+      const xml = "<>Hello</>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. No tag name found."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error if the tag name is empty but an attribute is set", () => {
+      const xml = '< class="name">Hello</>';
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. No tag name found."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error when encountering = character inside a tag", () => {
+      const xml = "<p = >Hello</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Invalid character encountered."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error when encountering = character inside a tag between attributes", () => {
+      const xml = "<p bold = invert>Hello</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Invalid character encountered."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error when attribute value is not inside quotes", () => {
+      const xml = "<p bold=true>Hello</p>";
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. Attribute values must be enclosed in double quotes."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
+    });
+
+    it("should throw an error when attribute value does not have a quote at the end", () => {
+      const xml = '<p bold="true>Hello</p>';
+
+      expect(() => parseXml(xml)).toThrowError(
+        "Invalid XML. XML closing tag is missing. Expected a close tag for 'p' before the end of the document."
+      );
+
+      const err = catchErr(() => parseXml(xml));
+      expect(err?.toString()).toMatchSnapshot();
     });
   });
 });
